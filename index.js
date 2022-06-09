51ef3a78e2de0d828e1bdca591cfb698da6006e3
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-06-09 12:42:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+  constructor(name) {
+    this.name = name;
+    }
+    speak() { if (this.name === 'Pablo') {
+      return `It's me! ${this.name}, the parrot!`;
+    } else 
+      return `${this.name} says squawk!`;
+    }
+    }
